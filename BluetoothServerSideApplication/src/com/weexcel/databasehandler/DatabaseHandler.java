eaedd270df7bc202a1eb1e3d26aa0de2cdc008c6
harshgaba08 <harsh.gaba@weexcelindia.com>
2014-08-08 12:28:56 +0530

    test
    test

--- BluetoothServerSideApplication/src/com/weexcel/databasehandler/DatabaseHandler.java
+++ BluetoothServerSideApplication/src/com/weexcel/databasehandler/DatabaseHandler.java
@@ -8,6 +8,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+     
+
+
 
 public class DatabaseHandler extends SQLiteOpenHelper
 {
