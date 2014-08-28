b3d04948280b7517804b0afdf67f66043c0eb7a8
harshgaba08 <harsh.gaba@weexcelindia.com>
2014-08-28 11:13:16 +0530

    wifi module updated image receiving
    image and client message receiving through wifi direct

--- BluetoothServerSideApplication/src/com/example/bluetoothserversideapplication/DeviceDetailFragment.java
+++ BluetoothServerSideApplication/src/com/example/bluetoothserversideapplication/DeviceDetailFragment.java
@@ -2,6 +2,7 @@ package com.example.bluetoothserversideapplication;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,10 +14,14 @@ import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
@@ -26,8 +31,11 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import com.example.bluetoothserversideapplication.DeviceListFragment.DeviceActionListener;
 import com.weexcel.databasehandler.DatabaseHandler;
@@ -47,16 +55,19 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
 	public static  DatabaseHandler db;
 	static String  clientname="";
 	static	String clientaddress="";
+	static	String clientmessage="";
+static boolean	issendrunning;
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
 
+   
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    	 issendrunning=true;
         mContentView = inflater.inflate(R.layout.device_detail, null);
-        
+       
         cc=new WifiClientModel();
         db=new DatabaseHandler(getActivity()); 
        
@@ -76,12 +87,21 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
         return mContentView;
     }
 
+   
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 //        ignore for now
     }
-
+@Override
+public void onResume() {
+if (issendrunning==false) {
+	new SendImage(getActivity(), mContentView.findViewById(R.id.device_address))
+    .execute();
+}
+	// TODO Auto-generated method stub
+	super.onResume();
+}
     @Override
     public void onConnectionInfoAvailable(final WifiP2pInfo info) {
         if (progressDialog != null && progressDialog.isShowing()) {
@@ -106,6 +126,8 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
         if (info.groupFormed && info.isGroupOwner) {
             new FileServerAsyncTask(getActivity(), mContentView.findViewById(R.id.device_address))
                     .execute();
+            new SendImage(getActivity(), mContentView.findViewById(R.id.device_address))
+            .execute();
         } else if (info.groupFormed) {
         	//ignore for now
         }
@@ -233,6 +255,7 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
             	String[] aa=result.split("/");
             	clientname=aa[0];
             	clientaddress=aa[1];
+            	clientmessage=aa[2];
                 statusText.setText(clientname+"\n"+clientaddress);
                 key++;
                 cc.setKEY_ID(key+""); // set the key id
@@ -240,7 +263,53 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
                 cc.setIP_ADDRESS(clientaddress);//set the client address
                 cc.setTIME(system_date_and_time);//set the time
                 db.addwifiClient(cc);// stores the data in the database
+            ////////////////////////////////////////////////
                 
+                
+//    			final Dialog dialog = new Dialog(context);
+//    			dialog.setContentView(R.layout.customdialog);
+//    			dialog.setTitle("Message From Client");
+//     
+//    			// set the custom dialog components - text, image and button
+//    			TextView text = (TextView) dialog.findViewById(R.id.text);
+//    			text.setText(clientmessage);
+//    			
+//     
+//    			Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+//    			// if button is clicked, close the custom dialog
+//    			dialogButton.setOnClickListener(new OnClickListener() {
+//    				@Override
+//    				public void onClick(View v) {
+//    					dialog.dismiss();
+//    				}
+//    			});
+//     
+//    			dialog.show();
+//                
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+             // set title
+             alertDialogBuilder.setTitle("Message From Client");
+
+             // set dialog message
+             alertDialogBuilder
+             	.setMessage(clientmessage)
+             	.setCancelable(false)
+             	
+             	.setNegativeButton("No",new DialogInterface.OnClickListener() {
+             		public void onClick(DialogInterface dialog,int id) {
+             			// if this button is clicked, just close
+             			// the dialog box and do nothing
+             			dialog.cancel();
+             		}
+             });
+
+             // create alert dialog
+             AlertDialog alertDialog = alertDialogBuilder.create();
+
+             // show it
+             alertDialog.show();
+                //////////////////////////////////////////////////
             }
 
         }
@@ -256,6 +325,93 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
 
     }
 
+    
+    public static class SendImage extends AsyncTask<Void, Void, String>{
+    	private Context context;
+        private TextView statusText;
+        private boolean running=false;
+        public boolean isRunning() {
+			return running;
+		}
+		/**
+         * @param context
+         * @param statusText
+         */
+        public SendImage(Context context, View statusText) {
+            this.context = context;
+            this.statusText = (TextView) statusText;
+        }
+		@Override
+		protected String doInBackground(Void... params) {
+			try {
+				
+                ServerSocket serverSocket1 = new ServerSocket(8988);
+             //   Log.d(WiFiDirectActivity.TAG, "Server: Socket opened");
+                
+                	
+                
+                Socket client = serverSocket1.accept();
+               // Log.d(WiFiDirectActivity.TAG, "Server: connection done");
+                final File f = new File(Environment.getExternalStorageDirectory() + "/"
+                        + context.getPackageName() + "/wifip2pshared-" + System.currentTimeMillis()
+                        + ".jpg");
+
+                File dirs = new File(f.getParent());
+                if (!dirs.exists())
+                    dirs.mkdirs();
+                f.createNewFile();
+
+              //  Log.d(WiFiDirectActivity.TAG, "server: copying files " + f.toString());
+                InputStream inputstream = client.getInputStream();
+               
+                fileWrite(inputstream, new FileOutputStream(f));
+                issendrunning=true;
+                serverSocket1.close();
+                Log.e("harsh.....",f.getAbsolutePath());
+                
+                	return f.getAbsolutePath();	
+               
+            } catch (IOException e) {
+              //  Log.e(WiFiDirectActivity.TAG, e.getMessage());
+                return null;
+            }
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
+			running=false;
+		}
+        @Override
+        protected void onPostExecute(String result) {
+            running=false;
+        	if (result != null) {
+               // statusText.setText("File copied - " + result);
+            	
+                Intent intent = new Intent();
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse("file://" + result), "image/*");
+                context.startActivity(intent);
+                issendrunning=false;
+            }
+
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see android.os.AsyncTask#onPreExecute()
+         */
+        @Override
+        protected void onPreExecute() {
+           // statusText.setText("Opening a server socket");
+        	running=true;
+        }
+    }
    
     public static boolean copyFile(InputStream inputStream, OutputStream out) {
         byte buf[] = new byte[1024];
@@ -278,7 +434,26 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
         return true;
     }
 
-	
+    public static boolean fileWrite(InputStream inputStream, OutputStream out) {
+        byte buf[] = new byte[1024];
+        int len;
+        long startTime=System.currentTimeMillis();
+        
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            inputStream.close();
+            long endTime=System.currentTimeMillis()-startTime;
+            Log.v("","Time taken to transfer all bytes is harsh.... : "+endTime);
+            
+        } catch (IOException e) {
+            Log.d(MainActivity.TAG, e.toString());
+            return false;
+        }
+        return true;
+    }
 
 }
 
--- BluetoothServerSideApplication/src/com/example/bluetoothserversideapplication/SendImageService.java
+++ BluetoothServerSideApplication/src/com/example/bluetoothserversideapplication/SendImageService.java
@@ -1,36 +1,42 @@
 package com.example.bluetoothserversideapplication;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+
 import android.app.IntentService;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 
 
-public class FileTransferService extends IntentService {
+public class SendImageService extends IntentService {
 
-    private static final int SOCKET_TIMEOUT = 5000;
+    public SendImageService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+public SendImageService(){
+	super("SendImageService");
+}
+
+
+	private static final int SOCKET_TIMEOUT = 5000;
     public static final String ACTION_SEND_FILE = "com.example.android.wifidirect.SEND_FILE";
     public static final String EXTRAS_FILE_PATH = "file_url";
     public static final String EXTRAS_GROUP_OWNER_ADDRESS = "go_host";
     public static final String EXTRAS_GROUP_OWNER_PORT = "go_port";
 
-    public FileTransferService(String name) {
-        super(name);
-    }
+   
 
-    public FileTransferService() {
-        super("FileTransferService");
-    }
-
+    
     /*
      * (non-Javadoc)
      * @see android.app.IntentService#onHandleIntent(android.content.Intent)
@@ -46,23 +52,23 @@ public class FileTransferService extends IntentService {
             int port = intent.getExtras().getInt(EXTRAS_GROUP_OWNER_PORT);
 
             try {
-                Log.d(MainActivity.TAG, "Opening client socket - ");
+                Log.d("client socket", "Opening client socket - ");
                 socket.bind(null);
                 socket.connect((new InetSocketAddress(host, port)), SOCKET_TIMEOUT);
 
-                Log.d(MainActivity.TAG, "Client socket - " + socket.isConnected());
+                Log.d("client socket connected", "Client socket - " + socket.isConnected());
                 OutputStream stream = socket.getOutputStream();
                 ContentResolver cr = context.getContentResolver();
                 InputStream is = null;
                 try {
-                    is =new ByteArrayInputStream(fileUri.getBytes());
-                } catch (Exception e) {
-                    Log.d(MainActivity.TAG, e.toString());
+                    is = cr.openInputStream(Uri.parse(fileUri));
+                } catch (FileNotFoundException e) {
+                    Log.d("harsh", e.toString());
                 }
-               // DeviceDetailFragment.copyFile(is, stream);
-                Log.d(MainActivity.TAG, "Client: Data written");
+                DeviceDetailFragment.copyFile(is, stream);
+                Log.d("harsh", "Client: Data written");
             } catch (IOException e) {
-                Log.e(MainActivity.TAG, e.getMessage());
+                Log.e("harsh", e.getMessage());
             } finally {
                 if (socket != null) {
                     if (socket.isConnected()) {
